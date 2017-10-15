52ae381fb2dbcf729aeac63c866c9b9e0e593d23
GregoryDorrifourt <gd.github@gmail.com>
2017-10-15 17:20:27 +0200

    convert project to sass + bootstrap 4 beta

--- src/app/dad/dad.component.ts
+++ src/app/dad/dad.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-dad',
   templateUrl: './dad.component.html',
-  styleUrls: ['./dad.component.css']
+  styleUrls: ['./dad.component.scss']
 })
 export class DadComponent implements OnInit {
 
